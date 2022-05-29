394a3f0bbbb5897aea9570dae8f3a067f03c9a27
NHL04 <huuluanofficial@gmail.com>
2022-05-29 21:54:55 +0700

    current-db-mysql

---
+++ DB/db mysql/mydb_shopee_item.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shopee_item`
+--
+
+DROP TABLE IF EXISTS `shopee_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shopee_item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(255) DEFAULT NULL,
+  `seller` varchar(255) DEFAULT NULL,
+  `price` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopee_item`
+--
+
+LOCK TABLES `shopee_item` WRITE;
+/*!40000 ALTER TABLE `shopee_item` DISABLE KEYS */;
+INSERT INTO `shopee_item` VALUES (1,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','chu shop 1','65000 VND','ao_nam'),(2,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 2','196000 VND','ao_nam'),(3,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 3','696969 VND','ao_nam'),(4,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 3','250 VND','ao_nam'),(5,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 6','90000VND','ao_nam'),(6,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 5','60000 VND','a0_nam'),(7,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 19','21000 VND','ao_nam'),(8,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 4','50000 VND','ao_nam'),(9,'Áo thun nam nữ unisex tay lỡ, áo phông cotton from rộng AD69 Oversize','shop 5','20000 VND','ao_nam');
+/*!40000 ALTER TABLE `shopee_item` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-29 21:54:11
